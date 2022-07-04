520a8cfc6ba69ea6e692aed623c0292fcd45b8c2
moyarka <mahyar.karimi2079@gmail.com>
2022-07-04 15:10:15 +0430

    add tests for is_configuration

--- estrest/test/test_event_structure.py
+++ estrest/test/test_event_structure.py
@@ -1,8 +1,9 @@
 import unittest
 
-from event_structure import EventStructure
-from src import ids
-from utils import list_ids
+from estrest.src.event import Event
+from estrest.src.event_structure import EventStructure
+from estrest.src import ids
+from estrest.src.utils import list_ids
 
 
 class TestEventStructure(unittest.TestCase):
@@ -48,6 +49,52 @@ class TestEventStructure(unittest.TestCase):
         p = EventStructure().prefix('a').times(EventStructure().prefix('b'))
         p = p.relabel({('a', '*'): 'x', ('*', 'b'): 'y'})
         self.assertEqual(p.get_labels(), {'(a, b)', 'x', 'y'})
+
+    def test_is_configuration_base(self):
+        es = EventStructure()
+        a, b = Event('a'), Event('b')
+        es.events = {a, b}
+        es.conflict = {a: set(), b: set()}
+        es.enabling = {a: [set()], b: [{a}]}
+
+        self.assertTrue(es.is_configuration({a, b}))
+
+    def test_is_configuration_conflict(self):
+        es = EventStructure()
+        a, b = Event('a'), Event('b')
+        es.events = {a, b}
+        es.conflict = {a: {b}, b: {a}}
+        es.enabling = {a: [set()], b: [set()]}
+
+        self.assertFalse(es.is_configuration({a, b}))
+
+    def test_is_configuration_not_secured(self):
+        es = EventStructure()
+        a, b, c = Event('a'), Event('b'), Event('c')
+        es.events = {a, b, c}
+        es.conflict = {a: set(), b: set(), c: set()}
+        es.enabling = {a: [set()], b: [{a}], c: [{a, b}]}
+
+        self.assertFalse(es.is_configuration({a, c}))
+        self.assertTrue(es.is_configuration({a, b, c}))
+
+    def test_is_configuration_real(self):
+        es = EventStructure()
+        a, b, c, d, e = [Event(x) for x in 'abcde']
+        es.events = {a, b, c, d, e}
+        es.conflict = {a: set(), b: {d, e}, c: set(), d: {b}, e: {b}}
+        es.enabling = {
+            a: [set()],
+            b: [{a}],
+            d: [{a}],
+            e: [{a, d}],
+            c: [{a, b}, {a, d, e}]
+        }
+
+        self.assertTrue(es.is_configuration({a, c, d, e}))
+        self.assertTrue(es.is_configuration({a, b, c}))
+
+        self.assertFalse(es.is_configuration({a, b, c, d}))
 
 
 if __name__ == '__main__':
