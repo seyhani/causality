f7847e9953c032042120dbbb40714a8ab538bb82
moyarka <mahyar.karimi2079@gmail.com>
2022-11-23 01:02:44 +0330

    update run script

--- estrest/draft/run.sh
+++ estrest/draft/run.sh
@@ -1,9 +1,19 @@
-echo "n,N" > data
-for i in {5..14}; do
+DATA_ALL=data_c12_pi_all.csv
+DATA_HALF=data_c12_pi_half.csv
+PATHVERT=pathvert/pathvert.old.py
+echo "n,N" > $DATA_ALL
+echo "n,N" > $DATA_HALF
+for i in {3..14}; do
     printf "Running for $i... "
-    printf "$i\n" |
+    printf "$i\nall\n" |
         python cm.py |
-        python pathvert/pathvert.py |
-        python -c "a = [int(x) for x in input().split()]; print(f'{$i},{sum(a)/len(a)}')" >> data
+        python $PATHVERT |
+        python -c "a = [int(x) for x in input().split()]; print(f'{$i},{sum(a)/len(a)}')" >> $DATA_ALL
+        printf "all, "
+    printf "$i\nhalf\n" |
+        python cm.py |
+        python $PATHVERT |
+        python -c "a = [int(x) for x in input().split()]; print(f'{$i},{sum(a)/len(a)}')" >> $DATA_HALF
+        printf "half, "
     printf "done!\n"
 done
