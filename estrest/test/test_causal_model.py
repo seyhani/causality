bf82a05d728908428d4d9d13459b0fc740a8fc3e
moyarka <mahyar.karimi2079@gmail.com>
2023-01-26 16:54:12 +0330

    update tests to use new method names, add new test for W-projection

--- estrest/test/test_causal_model.py
+++ estrest/test/test_causal_model.py
@@ -2,6 +2,8 @@ import unittest
 
 from causality.causal_model import CausalModel, PrimitiveEvent
 
+from causal_models import SUZY_BILLY
+
 
 class TestCausalModel(unittest.TestCase):
 
@@ -35,23 +37,13 @@ class TestCausalModel(unittest.TestCase):
         m.add('A', lambda vals: True, [])
         self.assertRaises(Exception, lambda: m.intervene({'X': True}))
 
-    def test_w_projection_vars(self):
-        m = CausalModel()
-        m.add('A', lambda vals: True, [])
-        m.add('B', lambda vals: False, [])
-        m.add('C', lambda vals: vals['A'] and vals['B'], ['A', 'B'])
-        m.add('D', lambda vals: vals['C'] and True, ['C'])
-
-        wp = m._w_projection_vars('A', 'D')
-        self.assertEqual(wp, {'A', 'B', 'C', 'D'})
-
     def test_w_projection(self):
         m = CausalModel()
         m.add('A', lambda vals: True, [])
         m.add('B', lambda vals: False, [])
         m.add('C', lambda vals: vals['A'] and vals['B'], ['A', 'B'])
         m.add('D', lambda vals: vals['C'] and True, ['C'])
-        m_wp = m.w_projection(
+        m_wp = m.get_w_projection(
             PrimitiveEvent('A', True), PrimitiveEvent('D', True)
         )
 
@@ -61,6 +53,16 @@ class TestCausalModel(unittest.TestCase):
         self.assertEqual(m_wp.vals['B'], False)
         self.assertEqual(m_wp.vals['C'], False)
         self.assertEqual(m_wp.vals['D'], False)
+
+    def test_w_projection_suzy_billy_billy_is_not_cause(self):
+        m = SUZY_BILLY
+        m_wp = m.get_w_projection(
+            PrimitiveEvent('BT', True), PrimitiveEvent('BS', True)
+        )
+
+        self.assertNotIn('ST', m_wp.vals)
+        self.assertNotIn('ST', m_wp.fns)
+        self.assertNotIn('ST', m_wp.deps)
 
 
 if __name__ == '__main__':
