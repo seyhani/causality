d3849cff495ef764cdfe3c64735f5621c38be976
mahykari <mahyar.karimi2079@gmail.com>
2023-03-11 20:35:34 +0330

    update previous test and add new ones

--- estrest/test/test_dep_finder.py
+++ estrest/test/test_dep_finder.py
@@ -4,11 +4,28 @@ from causality import DependencyFinder
 
 
 class TestDependencyFinder(unittest.TestCase):
-    
-    def test_sound_and_complete(self):
-        f = lambda vals: vals['A'] & vals['B']
+
+    def test_simple(self):
+        def f(vals): return vals['A'] & vals['B']
         result = DependencyFinder().find(f)
-        
-        self.assertIn('A', result)
-        self.assertIn('B', result)
-        self.assertNotIn('C', result)
+
+        self.assertEqual({'A', 'B'}, result)
+
+    def test_complex(self):
+        def f(vals):
+            return vals['A'] & vals['B'] | (not vals['C']) ^ vals['D']
+        result = DependencyFinder().find(f)
+        self.assertEqual({'A', 'B', 'C', 'D'}, result)
+
+    def test_nested(self):
+        def f(vals):
+            return g(vals) | h(vals)
+
+        def g(vals):
+            return vals['A'] & vals['B']
+
+        def h(vals):
+            return (not vals['B']) | vals['C']
+
+        result = DependencyFinder().find(f)
+        self.assertEqual({'A', 'B', 'C'}, result)
