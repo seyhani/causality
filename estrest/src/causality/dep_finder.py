87845ed7b65efeac11d6efd4463ee5111d2532d5
mahykari <mahyar.karimi2079@gmail.com>
2023-03-12 00:31:47 +0330

    Simplify dep_finder, remove cyclic import

--- estrest/src/causality/dep_finder.py
+++ estrest/src/causality/dep_finder.py
@@ -1,16 +1,15 @@
 from typing import Callable, Set
 
-from causality.causal_model import VALS
-
 
 class DependencyFinder:
     def __init__(self):
+        super().__init__()
         self.visited = set()
     
     def __getitem__(self, x: str) -> bool:
         self.visited.add(x)
         return False
   
-    def find(self, f: Callable[[VALS], bool]) -> Set[str]:
+    def find(self, f: Callable) -> Set[str]:
         f(self)
         return self.visited
